dbcf6d3a5815db7eb88071b75baefdc118019841
Raul Baltasar <r.baltasarb@gmail.com>
2019-07-14 20:28:28 +0200

    Fixed bug when no GUI is used

--- master_python/visualization.py
+++ master_python/visualization.py
@@ -377,7 +377,7 @@ samples_per_frame = int(config.MIC_RATE / config.FPS)
 # Array containing the rolling audio sample window
 y_roll = np.random.rand(config.N_ROLLING_HISTORY, samples_per_frame) / 1e16
 
-visualization_effect = visualize_energy_spectrum
+visualization_effect = visualize_energy
 """Visualization effect to display on the LED strip"""
 
 # Global microphone
